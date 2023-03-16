88c923ad7154a2762d9e060998ba6f08e37b1932
DylanoBoellaard <336751@student.mboutrecht.nl>
2023-03-16 13:36:20 +0100

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER','root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc-oop-toets');
+
+define('URLROOT', 'http://mvc-oop-toets.com');
+define('APPROOT', dirname(dirname(__FILE__)));
